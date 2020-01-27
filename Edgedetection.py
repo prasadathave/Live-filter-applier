f6b871314fc9c45a75864c9f9d327465f30ba186
prasadathave <44859397+prasadathave@users.noreply.github.com>
2020-01-27 19:41:48 +0530

    Create Edgedetection.py

---
+++ Edgedetection.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # the process of capturing video s basically capturing frames
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #GaussianBlur
+    gray = cv2.GaussianBlur(img,(5,5),0)
+    #Laplacian
+    Lapl = cv2.Laplacian(gray,cv2.CV_64F)
+    # Display the resulting frame
+    cv2.imshow('frame',Lapl)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
